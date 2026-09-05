4c79788a1e68c88429758d2976270fc9f0e2194b
sonu kumar <kumarsonu80080@gmail.in>
2026-09-05 18:34:05 +0530

    first commit

---
+++ day_1.py
@@ -0,0 +1,15 @@
+# print("hello world")
+# age = input("enter age")
+# print("my age is ",age)
+
+
+# ** day 2 **
+
+# marks = float(input("enter marks"))
+# print(marks)
+
+# a = 10
+# b = 12
+# print(a>)
+
+
