355a7abc3582e40b369d64e7d95f8179b8f78535
sonu kumar <kumarsonu80080@gmail.in>
2026-09-08 17:56:43 +0530

    loging system

---
+++ internship_assignment/logingSystem&Password.py
@@ -0,0 +1,34 @@
+def loging():
+    creatUserName = input("ENTER USERNAME : ")
+    creatPassword = int(input("ENTER PASSWORD : "))
+    print("\n")
+    print("*** LOGING ***\n")
+    
+
+    
+    i = 1
+    
+    while i<4 :
+        userName = input("enter user name : ")
+        password = int(input("enter password : "))
+        
+        if creatUserName == userName and creatPassword == password :
+            print("loging successfully")
+            break
+            
+        else:
+            print("Incorrect Username & Password")
+            print(f"Attampts Remaining {3-i}")
+            i+=1
+        if i >3:
+            print("----Account Locked----")
+    
+            
+            
+        
+
+
+
+# logingSystem = loging()
+# print(logingSystem)
+loging()
